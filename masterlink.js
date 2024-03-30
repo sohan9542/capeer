f857a1eda2ac00533c37e85d168d8d725c3c1bb0
sohan9542 <sohanurrahmants@gmail.com>
2024-03-30 14:40:10 +0600

    unusual code removed

--- masterlink.js
+++
@@ -1,5 +0,0 @@
-export const i_url = {
-    tradeZella: 'https://links.tradingjournal.com/tradezella',
-    traderVue: 'https://links.tradingjournal.com/tradervue',
-    traderSync:  'https://links.tradingjournal.com/tradersync'
-}
